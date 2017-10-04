de5de45a150a00ff16086168fae9e5b5e6eee179
simona <362732552@qq.com>
2017-10-04 13:38:46 +0800

    Ajax-likes-done

--- rango/models.py
+++ rango/models.py
@@ -25,6 +25,7 @@ class Page(models.Model):
     title = models.CharField(max_length=128)
     url = models.URLField(max_length=200)
     views = models.IntegerField(default=0)
+    
 
     def __str__(self):
         return self.title
--- static/bootstrap/js/rango-jquery.js
+++ static/bootstrap/js/rango-jquery.js
@@ -3,11 +3,6 @@ $(document).ready(function () {
         alert("You clicked the button using JQuery!");
     });
 
-    $("p").hover(function () {
-        $(this).css('color', 'red');
-    }, function () {
-        $(this).css('color', 'blue');
-    });
 
     $("#about-btn").click(function (event) {
         msgstr = $("#msg").html();
